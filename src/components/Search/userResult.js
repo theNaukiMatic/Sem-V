c37197b3eb3649c2ae160ef7a72da3fa687c8591
theNaukiMatic <naukeshgoyal@gmail.com>
2020-11-08 04:37:56 +0530

    no results comp

--- src/components/Search/userResult.js
+++ src/components/Search/userResult.js
@@ -11,6 +11,7 @@ import {
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Loading from "../LoadingComponent";
+import NightsStayIcon from "@material-ui/icons/NightsStay";
 
 const useStyles = makeStyles({
 	root: {
@@ -20,6 +21,34 @@ const useStyles = makeStyles({
 		height: 140,
 	},
 });
+
+const NoResults = () => {
+	const classes = useStyles();
+	return (
+		<Box>
+			<Card className={classes.cardPadding}>
+				<CardContent p={2}>
+					<Typography variant="h4" align="center">
+						<NightsStayIcon
+							style={{
+								fontSize: 100,
+								color: "gray",
+								textAlign: "center",
+							}}
+						/>
+					</Typography>
+					<Typography
+						variant="h5"
+						align="center"
+						style={{ color: "gray", padding: "20" }}
+					>
+						No search Results
+					</Typography>
+				</CardContent>
+			</Card>
+		</Box>
+	);
+};
 
 const SearchUserRes = () => {
 	const classes = useStyles();
@@ -34,6 +63,13 @@ const SearchUserRes = () => {
 	}, [userData]);
 	if (showLoading) {
 		return <Loading />;
+	} else if (userData.userData.length === 0) {
+		return (
+			<Box>
+				<br />
+				<NoResults />
+			</Box>
+		);
 	} else {
 		return (
 			<Box>
